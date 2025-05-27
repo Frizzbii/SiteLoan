c6a2a0be18753ff8367435a616d593a6c2c325ee
Tom Grandjean <tom.grandjean@rivagroup.com>
2025-05-27 14:41:39 +0200

    typewriting

--- script.js
+++ script.js
@@ -8,3 +8,48 @@ document.querySelectorAll('.competences').forEach((competence) => {
     competence.classList.toggle('open');
   });
 });
+
+const texts = ["Un projet ?", "Des questions ?", "Contactez moi !"];
+const typingSpeed = 100;
+    const deletingSpeed = 50;
+    const delayBetweenWords = 2000;
+
+    let index = 0;
+    let subIndex = 0;
+    let deleting = false;
+
+    const typewriterElement = document.getElementById("typewriter-text");
+    const h2Element = document.getElementById("typewriter");
+
+    function type() {
+      const currentText = texts[index];
+
+      if (index === 2) {
+            typewriterElement.classList.add("underlined");
+          }
+      else {
+            typewriterElement.classList.remove("underlined");
+      }
+
+      // Update text content
+      if (!deleting) {
+        typewriterElement.textContent = currentText.substring(0, subIndex + 1);
+        subIndex++;
+        if (subIndex === currentText.length) {
+          deleting = true;
+          setTimeout(type, delayBetweenWords);
+          return;
+        }
+      } else {
+        typewriterElement.textContent = currentText.substring(0, subIndex - 1);
+        subIndex--;
+        if (subIndex === 0) {
+          deleting = false;
+          index = (index + 1) % texts.length;
+        }
+      }
+
+      setTimeout(type, deleting ? deletingSpeed : typingSpeed);
+    }
+
+    type();
